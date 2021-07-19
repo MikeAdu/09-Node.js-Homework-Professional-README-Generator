57e126e27c7e878884c1fe27835f6233cfb9cc94
MikeAdu <mike_adusah@yahoo.com>
2021-07-19 12:24:42 -0700

    added path

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
+const path = require('path')
 // TODO: Create an array of questions for user input
 const questions = [
     "What is your project title?",
@@ -15,7 +18,9 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) { }
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(),fileName),data)
+ }
 
 // TODO: Create a function to initialize app
 function init() { }
