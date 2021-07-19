31531a102aac1d7ea7c7c05fb901ba8f3f37e5d6
MikeAdu <mike_adusah@yahoo.com>
2021-07-19 11:48:35 -0700

    adding question promt

---
+++ index.js
@@ -0,0 +1,24 @@
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = [
+    "What is your project title?",
+    "Describe your project",
+    'what are your instalation instructions',
+    "Describe the usage information",
+    "what are the contribuations guidlines",
+    "What are the test instructions",
+    "What license does your project use",
+    "What is your github username?",
+    "What is your email?",
+
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) { }
+
+// TODO: Create a function to initialize app
+function init() { }
+
+// Function call to initialize app
+init();
